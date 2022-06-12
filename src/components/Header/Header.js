185c207a11b8a0980494d040a6a23bb286dcc137
Tusharbd5 <tushardebhata24@gmail.com>
2022-06-12 08:40:45 +0600

    Header file created

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,14 +1,25 @@
-import React from 'react';
 import CustomLink from '../CustomLink/CustomLink';
 import './Header.css';
+import xicon from '../../images/Icon/cross-icon.png';
+import oicon from '../../images/Icon/menu-icon.png'
+import { useState } from 'react';
 
 const Header = () => {
+    const handleToggle = () => {
+        const items = document.querySelector('.nav-items');
+        items.classList.toggle('open');
+
+    }
+
     return (
         <nav className='header'>
             <div className='logo'>
-                <h1 style={{ fontSize: "25px", margin: "0" }}>Self Teaching</h1>
-                <h1 style={{ color: "orange", fontSize: "30px", margin: "0" }}>Provider</h1>
+                <h3 className='text-white m-0 fw-light fs-4'>Self Teaching</h3>
+                <h1 style={{ color: "orange", fontSize: "25px", margin: "0" }}>Provider</h1>
             </div>
+            <button onClick={handleToggle} className='nav-toggle'>
+                <span></span>
+            </button>
             <div className='nav-items'>
                 <CustomLink to="/home">Home</CustomLink>
                 <CustomLink to="/services">Services</CustomLink>
